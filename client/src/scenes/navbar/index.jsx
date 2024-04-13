7ea9d037b5159600b2c2998c5df0c14b361d7c99
premgupta011 <premgupta0807@gmail.com>
2024-04-13 22:41:45 +0530

    udated

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
             },
           }}
         >
-          Sociopedia
+          VibeZone
         </Typography>
         {isNonMobileScreens && (
           <FlexBetween
