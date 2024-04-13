7900cdb42e65a0a9cd745c2d555c0490587542ac
premgupta011 <premgupta0807@gmail.com>
2024-04-13 17:20:06 +0530

    changed

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
             },
           }}
         >
-          Sociopedia
+          Premgram
         </Typography>
         {isNonMobileScreens && (
           <FlexBetween
